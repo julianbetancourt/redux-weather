cb8631c2399a0619db198654859df9854b31414e
julianbetancourt <julianbetancourt10@gmail.com>
2016-10-19 21:55:02 -0500

    add redux devtools enchancements

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,12 +1,18 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from '../reducers';
 import thunk from 'redux-thunk';
+
+
+const enhancer = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
 const configStore = (initialState) => {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    enhancer
   );
 }
 
